984255ce5ca76bace5df93a9d5af07a969277c85
2384320 <chdms1549@gmail.com>
2023-04-03 22:50:30 +0900

    [Upload] Chap01 OpenChallenge, Practice
    Upload Chap01 OpenChallenge, Practice01~02

---
+++ Chap01/src/main/java/OpenChallenge.java
@@ -0,0 +1,7 @@
+public class OpenChallenge {
+    public static void main(String[] args) {
+        System.out.println("Choeun Hyun");
+        System.out.println("23 years old");
+        System.out.println("Department of Computer Software Engineering");
+    }
+}
---
+++ Chap01/src/main/java/Practice02.java
@@ -0,0 +1,6 @@
+public class Practice02 {
+    public static void main(String[] args) {
+        System.out.println("Sorry~~");
+        System.out.println("재밌어서 미안합니다~~");
+    }
+}
